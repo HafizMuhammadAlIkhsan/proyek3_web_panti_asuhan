8304b71b8a2e0ca211e0d2cf6455801e478e5dd9
HafizMuhammadAlIkhsan <hafiz.muhammad.tif23@polban.ac.id>
2024-09-30 12:28:36 +0700

    :art: add users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('pekerjaan');
+            $table->integer('kontak', 13);
+            $table->char('kelamin', 1);
             $table->rememberToken();
             $table->timestamps();
         });
