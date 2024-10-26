fd7d8bc577eaea333e24e7d366e0a6e879c8b992
HafizMuhammadAlIkhsan <hafiz.muhammad.tif23@polban.ac.id>
2024-10-26 11:37:55 +0700

    add donatur controller

---
+++ app/Http/Controllers/DonaturController.php
@@ -0,0 +1,24 @@
+<?php
+// app/Http/Controllers/DonaturController.php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Donatur;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class DonaturController extends Controller
+{
+    public function index()
+    {
+        // Ambil data pengguna yang sedang login
+        $user = Auth::user();
+
+        // Ambil data donatur berdasarkan email (jika email adalah primary key)
+        $donatur = Donatur::findOrFail($user->email);
+        dd($user, $donatur);
+        return view('welcome', ['donatur' => $donatur]);
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginDonaturController;
 use App\Http\Controllers\LoginAdminController;
 use App\Http\Controllers\DonasiJasaController;
 
 Route::get('/login', [LoginAdminController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginAdminController::class, 'login']);
 
+Route::get('/Login_Admin', function () {
+    return view('Admin/loginadmin');
+});
+
+Route::get('/Beranda_Admin', function () {
+    return view('Admin/beranda_admin');
+});
+
 Route::post('/donasi-jasa/store', [DonasiJasaController::class, 'store'])->name('donasi-jasa.store');
 
-use App\Http\Controllers\LoginDonaturController;
-
-Route::get('/login', [LoginDonaturController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginDonaturController::class, 'login']);
-Route::post('/logout', [LoginDonaturController::class, 'logout'])->name('logout');
 Route::get('/input_jasa', function () {
     return view('Admin/input_jasa');
 });
@@ -39,6 +43,10 @@ Route::get('/Register2', [RegisterController::class, 'showRegister2'])->name('re
 // Route untuk proses register2 (menyimpan data tambahan)
 Route::post('/Register2', [RegisterController::class, 'postRegister2'])->name('register.step2.post');
 
+Route::get('/login', [LoginDonaturController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginDonaturController::class, 'login']);
+Route::post('/logout', [LoginDonaturController::class, 'logout'])->name('logout');
+
 Route::get('/Login', function () {
     return view('Masyarakat_Umum/login');
 });
@@ -51,28 +59,23 @@ Route::get('/Halaman_Donasi_Umum', function () {
     return view('Masyarakat_Umum/beranda_donasi_masyarakat_umum');
 })->name('hal_donasi_umum');
 
-Route::get('/beranda_donatur', function () {
-    return view('Donatur/beranda_donatur');
-})->name('beranda_donatur');
+use App\Http\Controllers\DonaturController;
 
-Route::get('/Halaman_Donasi', function () {
-    return view('Donatur/beranda_donasi');
-})->name('hal_donasi_donatur');
+Route::get('/donatur/{email}', [DonaturController::class, 'show']);
 
-Route::get('/Halaman_Donasi_Jasa', function () {
-    return view('Donatur/donatur_donasi_jasa');
-})->name('hal_donasi_jasa');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/beranda_donatur', function () {
+        return view('Donatur/beranda_donatur');
+    })->name('beranda_donatur');
 
+    Route::get('/Halaman_Donasi', function () {
+        return view('Donatur/beranda_donasi');
+    })->name('hal_donasi_donatur');
 
-Route::get('/Login_Admin', function () {
-    return view('Admin/loginadmin');
+    Route::get('/Halaman_Donasi_Jasa', function () {
+        return view('Donatur/donatur_donasi_jasa');
+    })->name('hal_donasi_jasa');
 });
-
-Route::get('/Beranda_Admin', function () {
-    return view('Admin/beranda_admin');
-});
-
-
 
 // Route::get('/Register1', function () {
 //     return view('Masyarakat_Umum/register1');
