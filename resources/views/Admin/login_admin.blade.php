efb3a91a46dc3e386f9f62dcce43e6546e97e37b
HafizMuhammadAlIkhsan <hafiz.muhammad.tif23@polban.ac.id>
2024-12-05 07:50:17 +0700

    ganti nama route untuk login admin

---
+++ resources/views/Admin/login_admin.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background: linear-gradient(to right, rgb(131, 78, 209), rgb(153, 61, 164), rgb(67,40,107));
+        }
+        .container {
+            display: flex;
+            max-width: 900px;
+            width: 100%;
+            background-color: #fff;
+            border-radius: 10px;
+            overflow: hidden;
+            box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+        }
+        .image-container {
+            width: 50%;
+            background: url('image/anakpanti.jpg') no-repeat center center;
+            background-size: cover;
+        }
+        .form-container {
+            width: 50%;
+            padding: 40px;
+            background-color: #eee;
+        }
+        .form-container h2 {
+            margin-bottom: 20px;
+            font-size: 24px;
+            text-align: center;
+        }
+        .form-container form {
+            display: flex;
+            flex-direction: column;
+        }
+        .form-container input {
+            margin-bottom: 15px;
+            padding: 10px;
+            font-size: 16px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+        .form-container button {
+            padding: 10px;
+            font-size: 16px;
+            background-color: #4CAF50;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        .form-container button:hover {
+            background-color: #45a049;
+        }
+        .form-container a {
+            text-align: center;
+            margin-top: 10px;
+            color: #666;
+            font-size: 14px;
+            text-decoration: none;
+        }
+        .form-container a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="container">
+        <div class="image-container"></div>
+        <div class="form-container">
+            <h2>Login Admin</h2>
+            <form action="{{ route('login_admin') }}" method="POST">
+                @csrf
+                <input type="text" name="email_admin" placeholder="Email atau Nama" required>
+                <input type="password" name="password_admin" placeholder="Password" required>
+                <button type="submit">Login</button>
+                <a href="#">Lupa password?</a>
+            </form>        
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    @if ($errors->has('email'))
+        <script>
+            Swal.fire({
+                icon: 'error',
+                title: 'Terjadi Kesalahan',
+                text: '{{ $errors->first('email') }}'
+            });
+        </script>
+    @endif
+    
+</body>
+</html>
