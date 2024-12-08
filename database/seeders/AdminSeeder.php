48292aefce4ee7932cbe20e5a59c8c421f264d42
HafizMuhammadAlIkhsan <hafiz.muhammad.tif23@polban.ac.id>
2024-12-08 14:20:27 +0700

    menambahkan data status akun pada AdminSeeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -13,42 +13,48 @@ class AdminSeeder extends Seeder
             'email_admin' => 'admin@example.com',
             'nama_pengurus' => 'Admin123',
             'password_admin' => bcrypt('password123'),
-            'jabatan' => 'Manager'
+            'jabatan' => 'Manager',
+            'status_akun' => 'Aktif'
         ]);
 
         DB::table('admin')->insert([
             'email_admin' => 'sulthan@example.com',
             'nama_pengurus' => 'Sulthan Aulia Rahman',
             'password_admin' => bcrypt('sulthan123'),
-            'jabatan' => 'Admin'
+            'jabatan' => 'Admin',
+            'status_akun' => 'Aktif'
         ]);
 
         DB::table('admin')->insert([
             'email_admin' => 'Jihan@example.com',
             'nama_pengurus' => 'JihanHumaira',
             'password_admin' => bcrypt('jihan123'),
-            'jabatan' => 'Admin'
+            'jabatan' => 'Admin',
+            'status_akun' => 'Aktif'
         ]);
         
         DB::table('admin')->insert([
             'email_admin' => 'hafiz@example.com',
             'nama_pengurus' => 'HafizMuhammadAlIkhsan',
             'password_admin' => bcrypt('hafiz123'),
-            'jabatan' => 'Admin'
+            'jabatan' => 'Admin',
+            'status_akun' => 'Aktif'
         ]);
 
         DB::table('admin')->insert([
             'email_admin' => 'agra@example.com',
             'nama_pengurus' => 'AgraAnisaIbtisamah',
             'password_admin' => bcrypt('agra123'),
-            'jabatan' => 'Admin'
+            'jabatan' => 'Admin',
+            'status_akun' => 'Aktif'
         ]);
 
         DB::table('admin')->insert([
             'email_admin' => 'ryuki@example.com',
             'nama_pengurus' => 'RyukiHagaBudiarto',
             'password_admin' => bcrypt('ryuki123'),
-            'jabatan' => 'Admin'
+            'jabatan' => 'Admin',
+            'status_akun' => 'Aktif'
         ]);
 
     }
